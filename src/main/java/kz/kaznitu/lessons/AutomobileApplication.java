506a2299dad004ff57d05b8a66a084f179f2f6f8
tim4k <tim4kz@gmail.com>
2018-05-04 09:11:32 +0600

    v 1

---
+++ src/main/java/kz/kaznitu/lessons/AutomobileApplication.java
@@ -0,0 +1,12 @@
+package kz.kaznitu.lessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobileApplication.class, args);
+	}
+}
